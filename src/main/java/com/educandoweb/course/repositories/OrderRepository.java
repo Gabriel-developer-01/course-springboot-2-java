f83f5943930bf4b57e2c547275879662f17f352b
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-07-14 22:54:42 -0300

    Category

--- src/main/java/com/educandoweb/course/repositories/OrderRepository.java
+++ src/main/java/com/educandoweb/course/repositories/OrderRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.course.entities.Order;
 
-public interface OrderRepository extends JpaRepository<Order, Long>{ // O UserRepository é responsável por fazer operações com a entidade User
+public interface OrderRepository extends JpaRepository<Order, Long>{ // O UserRepository é responsável por fazer operações com a entidade Order
       
 }

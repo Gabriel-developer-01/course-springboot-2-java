3dd0203ffb9b17f7f29767f27e334dbd4a690e0a
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-07-16 14:32:03 -0300

    Exception handling - findById

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
@@ -33,12 +34,12 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);
+		User entity = repository.getOne(id); // getOne esse método sempre retornará um proxy sem atingir o banco de dados 
 		UpdateData(entity, obj);
 		return repository.save(entity);
 	}
 
-	private void UpdateData(User entity, User obj) {
+	private void UpdateData(User entity, User obj) { // atualizar os dados
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
